9a5d0eb0abf38b64e1f242803d970caf693505a2
Tyson Lam <tysonlyt@gmail.com>
2026-05-15 15:40:06 +0800

    update wp-content

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"x#n<Sd#bAD[(c!okHTXUH>ygC6WPT0*EII}/Ji`h]7?(;C9b@ jToq576 `f@BI;";s:7:"version";s:5:"1.1.0";s:13:"attackDataKey";i:952;s:11:"wafDisabled";b:0;}
